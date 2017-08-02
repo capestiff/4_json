946586ddca4d0d6af8d2e019187f5e3772e048cf
Maxim Shafarostov <capestiff@ya.ru>
2017-08-02 09:02:01 +0300

    Refactoring names of the variables, deleted uneccessary file output.

--- pprint_json.py
+++ pprint_json.py
@@ -4,13 +4,13 @@ import argparse
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
-        data = json.load(file)
+        json_data_ = json.load(file)
     file.close()
-    return data
+    return json_data_
 
 
-def pretty_print_json(data):
-    print(json.dumps(json.loads(data), indent=4, ensure_ascii=False))
+def pretty_print_json(json_data_):
+    print(json.dumps(json.loads(json_data_), indent=4, ensure_ascii=False))
 
 
 def parse_cli_args():
@@ -24,5 +24,3 @@ if __name__ == '__main__':
 
     json_data = load_data(path_to_file)
     pretty_print_json(json_data)
-    with open('json.txt', 'w') as f:
-        f.write(json.dumps(json.loads(json_data), indent=4, ensure_ascii=False))
